e7d007d0d0f344f87f401dfd25664e697013a434
eddy <charley.borremans@gmail.com>
2016-02-22 02:20:47 +0100

    removed the 2 dice case since it is covered by the general case

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -14,21 +14,28 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        // Main Form
         public Form1()
         {
             InitializeComponent();
             recalculate();
         }
 
+        // Test function for calculating the total number of dots on a single die
         public int totalNrOfDotsOnAllSides(int sidesPerDie)
         {
             return (sidesPerDie + 1) * sidesPerDie / 2;
         }
 
+        //
+        // Recursive function to calculate the number of ways to throw 'totalNrOfDots' 
+        // with 'nrOfDice' Dice with each 'sidePerDie' sides per die.
+        //
         public int numberOfWaysToThrow(int totalNrOfDots, int nrOfDice, int sidesPerDie)
         {
             int nrOfWays = 0;
 
+            // The trivial case of a single die
             if (nrOfDice == 1)
             {
                 if (totalNrOfDots >= 1 && totalNrOfDots <= sidesPerDie)
@@ -36,15 +43,20 @@ namespace WindowsFormsApplication1
                     nrOfWays = 1;
                 }
             }
-            else if(nrOfDice == 2)
-            {
-                nrOfWays = Math.Min(nrOfDice * (sidesPerDie + 1) - totalNrOfDots, totalNrOfDots) - 1;
-                if (nrOfWays < 0) nrOfWays = 0;
-
-                Debug.WriteLine("nrOfDice: " + nrOfDice);
-                Debug.WriteLine("nrOfWays: " + nrOfWays);
-            }           
             else
+
+            // All other nr of dice are calculated recursively based on the 1 die case.
+            // 
+            // The number of ways to throw 'totalNrOfDots' with 'nrOfDice' Dice with each 'sidePerDie' sides per die is the same as
+            // The number of ways to throw ('totalNrOfDots' - nrOfDots) with ('nrOfDice' - 1) Dice with each 'sidePerDie' sides per 
+            // die for each of the 'nrOfDice'-th die
+            // For example, the number of ways to throw 5 with 3 dice is:
+            // the number of ways to throw 4 with 2 dies and with 3-rd die 1 => 3
+            // PLUS
+            // the number of ways to throw 3 with 2 dies and with 3-rd die 2 => 2
+            // PLKUS
+            // the number of ways to throw 2 with 2 dies and with 3-rd die 3 => 1
+            // which totals 3 + 2 + 1 = 6
             {
                 Debug.WriteLine("nrOfDice     : " + nrOfDice);
                 Debug.WriteLine("totalNrOfDots: " + totalNrOfDots);
